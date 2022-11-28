af79334739827c20fdce2f88c71830699eabbaec
kunal-kadam <62118171+kunal-kadam@users.noreply.github.com>
2022-11-28 12:00:57 +0530

    Create CreditLimit.sol

---
+++ CreditLimit.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract CreditLimit{
+    uint limit;
+
+
+function set(uint _limit) public {
+        limit = _limit;
+    }
+
+function travelling(uint travellExpense) public{
+    require(limit >= travellExpense,"You have exhausted your balance");
+    limit -= travellExpense;
+}
+
+function food(uint foodExpense) public{
+    require(limit >= foodExpense,"You have exhausted your balance");
+    limit -= foodExpense;
+}
+    
+function retrieveCredLimit () public view returns(uint){
+        return (limit);
+    }
+
+}
